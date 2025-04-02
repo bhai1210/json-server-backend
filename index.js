4767eca5c88a5cc8888144d338c68e21a63c0796
bhai1210 <rahulbhavsar1210@gmail.com>
2025-04-02 23:59:37 +0530

    final deploy

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-const jsonServer = require("json-server"); // importing json-server library
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router("db.json"); // ensure path is correct
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 5000; //  chose port from here like 8080, 3001
+const port = 5000;
+
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
+server.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+  });
+  
