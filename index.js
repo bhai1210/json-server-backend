5ec4ea48c3a40dcf7d469fd42358a9cfafa740bd
bhai1210 <rahulbhavsar1210@gmail.com>
2025-04-03 09:45:35 +0530

    kale

--- index.js
+++ index.js
@@ -5,15 +5,21 @@ const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = 5000;
 
+// Apply default middlewares (includes logger, static, CORS, etc.)
 server.use(middlewares);
+
+// Custom CORS setup (you already did this correctly, just make sure it's BEFORE router)
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Or specify your frontend origin for more security
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
+
+// Add router
 server.use(router);
 
+// Start server
 server.listen(port, () => {
-  console.log(`JSON Server is running on port ${port}`);
+  console.log(`JSON Server is running on http://localhost:${port}`);
 });
